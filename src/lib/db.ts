f4a9d05fcf5038208f84b33c3b6b69e98b55df3b
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-01-09 15:42:01 +0900

    init

---
+++ src/lib/db.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import {DataSource, Repository} from "typeorm";
+import {Prompt} from "@/db/entity/prompt";
+import {PromptVariable} from "@/db/entity/prompt_variable";
+import {AppDataSource} from "@/db/ormconfig";
+
+class PromptRepository {
+    dataSource: DataSource
+    prompt: Repository<Prompt>
+    promptVariable: Repository<PromptVariable>
+
+    constructor(dataSource: DataSource) {
+        this.dataSource = dataSource
+    }
+
+    async setUp(): Promise<void> {
+        if (!this.dataSource.isInitialized) {
+            await this.dataSource.initialize().then(() => console.log("DataSource initialized"))
+        }
+        if (!this.prompt) {
+            this.prompt = this.dataSource.getRepository(Prompt)
+        }
+        if (!this.promptVariable) {
+            this.promptVariable = this.dataSource.getRepository(PromptVariable)
+        }
+    }
+
+    async getAllPrompts(): Promise<Prompt[]> {
+        await this.setUp()
+        return this.prompt.find({
+            relations: ["promptVariables"],
+            where: {isActive: true}
+        });
+    }
+}
+
+export const db: PromptRepository = new PromptRepository(AppDataSource)
