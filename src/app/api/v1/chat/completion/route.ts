c7f2b77c5e461e54ce6ff42a1854778feadd0963
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-01-10 01:11:07 +0900

    init

---
+++ src/app/api/v1/chat/completion/route.ts
@@ -0,0 +1,15 @@
+import {ChatOpenAI} from "@langchain/openai";
+
+export async function POST(request: Request): Promise<Response> {
+    const data: FormData = await request.formData();
+    const templated = data.get('templated');
+    if (templated) {
+        const model = new ChatOpenAI();
+        const response = await model.invoke(templated.toString());
+        console.log(response);
+        return new Response(JSON.stringify(response), {status: 200});
+    } else {
+        return new Response("templated is required", {status: 400});
+    }
+
+}
