8dda33d717678fe8c0930dbe438227b9f458c26b
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-01-10 08:26:34 +0900

    init

--- src/app/prompts/[id]/edit/page.tsx
+++ src/app/prompts/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import {ReactElement} from "react";
 import {db} from "@/lib/data";
+import {DeleteVariablesButton} from "@/ui/button";
 
 export default async function Page({params}: { params: { id: string } }): Promise<ReactElement> {
     const prompt = await db.getPromptById(parseInt(params.id));
@@ -8,30 +9,50 @@ export default async function Page({params}: { params: { id: string } }): Promis
     return (
         <div>
             <div>
-                <h1 className={"font-extrabold p-2 bg-gray-400"}>parameters</h1>
+                <h1 className={"font-extrabold p-2 bg-gray-400"}>Variables</h1>
                 <table className={"border"}>
                     <thead>
                     <tr className={"border"}>
                         <th>Variable Name</th>
                         <th>Variable Type</th>
+                        <th></th>
                     </tr>
                     </thead>
                     <tbody>
                     {variables.map(variable => {
                         return (
                             <tr key={variable.name}>
-                                <td>{variable.name}</td>
+                                <td>{`{{${variable.name}}}`}</td>
                                 <td>{variable.type}</td>
+                                <td><DeleteVariablesButton id={variable.id}/></td>
                             </tr>
                         );
                     })}
                     </tbody>
                 </table>
+
+
+            </div>
+            <div>
+                <h1 className={"font-extrabold p-2 bg-gray-400"}>Add Variable</h1>
+                <form action={"/api/v1/prompt_variables"} method={"POST"} autoComplete={"false"}
+                      className={"flex flex-col"}>
+                    <input type="hidden" name="promptId" value={prompt.id}/>
+                    <label>Name</label>
+                    <input type="text" name="name" autoComplete={"false"} className={"border shadow p-2"}/>
+                    <label>Type</label>
+                    <select name="type" className={"border shadow p-2"}>
+                        <option value="string">string</option>
+                        <option value="number">number</option>
+                        <option value="text">text</option>
+                    </select>
+                    <button type="submit" className={"border shadow bg-blue-300"}>Add</button>
+                </form>
             </div>
 
             <div>
                 <h1 className={"font-extrabold p-2 bg-gray-400"}>Prompt</h1>
-                <form action={"/api/v1/prompts"} method={"POST"} className={"flex flex-col"}>
+                <form action={"/api/v1/prompts"} method={"POST"} autoComplete={"false"} className={"flex flex-col"}>
                     <input type="hidden" name="id" value={prompt.id}/>
                     <label>Name</label>
                     <input type="text" name="name" defaultValue={prompt.name} className={"border shadow p-2"}/>
