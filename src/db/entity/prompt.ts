79731f95c741e395d2f866f606b808498f24100c
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-01-09 15:23:52 +0900

    init

--- src/db/entity/prompt.ts
+++ src/db/entity/prompt.ts
@@ -1,12 +1,17 @@
 import "reflect-metadata";
 import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {IsDate} from "class-validator";
 import {PromptVariable} from "@/db/entity/prompt_variable";
 
 @Entity({name: 'prompts'})
 export class Prompt {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({
+        type: "varchar",
+        length: 256
+    })
+    name: string;
 
     @Column({
         type: "text",
@@ -17,24 +22,6 @@ export class Prompt {
     promptVariables: Promise<PromptVariable[]>;
 
     @Column({
-        type: "timestamp",
-        default: () => "CURRENT_TIMESTAMP",
-        name: "created_at"
-    })
-
-    @IsDate()
-    createdAt: Date;
-
-    @Column({
-        type: "timestamp",
-        default: () => "CURRENT_TIMESTAMP",
-        name: "updated_at"
-    })
-    @IsDate()
-    updatedAt: Date;
-
-    @Column({
-        type: "boolean",
         default: true,
         name: "is_active"
     })
